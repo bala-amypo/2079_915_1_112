2a8bd8838ce4b01cee408edfe69a3f89d544f6c4
bala-amypo <bala@amypo.in>
2025-12-20 09:25:55 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CredentialRecordServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CredentialRecordServiceImpl.java
@@ -1,48 +1,30 @@
 package com.example.demo.service.impl;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.CredentialRecord;
-import com.example.demo.entity.User;
-import com.example.demo.entity.VerificationRule;
-import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.CredentialRecordRepository;
-import com.example.demo.repository.UserRepository;
-import com.example.demo.repository.VerificationRuleRepository;
 import com.example.demo.service.CredentialRecordService;
 
 @Service
 public class CredentialRecordServiceImpl
         implements CredentialRecordService {
 
-    private final CredentialRecordRepository credentialRepo;
-    private final UserRepository userRepo;
-    private final VerificationRuleRepository ruleRepo;
+    private final CredentialRecordRepository repository;
 
-    public CredentialRecordServiceImpl(
-            CredentialRecordRepository credentialRepo,
-            UserRepository userRepo,
-            VerificationRuleRepository ruleRepo) {
-        this.credentialRepo = credentialRepo;
-        this.userRepo = userRepo;
-        this.ruleRepo = ruleRepo;
+    public CredentialRecordServiceImpl(CredentialRecordRepository repository) {
+        this.repository = repository;
     }
 
     @Override
-    public CredentialRecord createCredential(CredentialRecord record) {
+    public CredentialRecord save(CredentialRecord record) {
+        return repository.save(record);
+    }
 
-        User user = userRepo.findById(record.getUser().getId())
-                .orElseThrow(() ->
-                        new ResourceNotFoundException("User not found"));
-
-        VerificationRule rule = ruleRepo
-                .findById(record.getVerificationRule().getId())
-                .orElseThrow(() ->
-                        new ResourceNotFoundException("Rule not found"));
-
-        record.setUser(user);
-        record.setVerificationRule(rule);
-
-        return credentialRepo.save(record);
+    @Override
+    public List<CredentialRecord> getByUserId(Long userId) {
+        return repository.findByUserId(userId);
     }
 }
