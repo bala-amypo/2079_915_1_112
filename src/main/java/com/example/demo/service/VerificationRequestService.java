7e7d441a52bea93b556799f39c8097692b510bdd
bala-amypo <bala@amypo.in>
2025-12-24 06:37:44 +0000

    User commit

--- src/main/java/com/example/demo/service/VerificationRequestService.java
+++ src/main/java/com/example/demo/service/VerificationRequestService.java
@@ -1,11 +1,9 @@
 package com.example.demo.service;
 
-import java.util.List;
-
 import com.example.demo.entity.VerificationRequest;
 
 public interface VerificationRequestService {
+
     VerificationRequest initiateVerification(VerificationRequest request);
-    VerificationRequest processVerification(Long requestId);
-    List<VerificationRequest> getRequestsByCredential(Long credentialId);
+
 }
