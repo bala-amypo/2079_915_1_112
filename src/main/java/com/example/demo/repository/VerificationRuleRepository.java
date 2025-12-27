8f2c2c6ec2621d28ce3e42889998a8573384f33a
bala-amypo <bala@amypo.in>
2025-12-27 07:30:21 +0000

    User commit

--- src/main/java/com/example/demo/repository/VerificationRuleRepository.java
+++ src/main/java/com/example/demo/repository/VerificationRuleRepository.java
@@ -2,7 +2,9 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.VerificationRule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VerificationRuleRepository
         extends JpaRepository<VerificationRule, Long> {
 }
